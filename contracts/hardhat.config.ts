10c4f32da61a0187539f29498033fd2c8c4c0e7f
Krisztian Barta <bartariki@gmail.com>
2024-08-11 15:15:34 +0200

    Very last commit

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,35 +1,36 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 
-module.exports = {
+const PRIVATE_KEY = process.env.PRIVATE_KEY_METAMASK0 || "1cbd3cf2dcf13b0c0dfdf13d1069475cb10cd4fff495a1c67a104f3e4f8e4ef1";
+
+const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
-    //Add extra chains as needed 
-    hardhat: {
-      chainId: 11155111,
-    },
-    baseSepolia: {
-      url: `${process.env.ALCHEMY_BASE_SEPOLIA_URL}`,
-      accounts: [`0x${process.env.PRIVATE_KEY_METAMASK0}`,
-                `0x${process.env.PRIVATE_KEY_METAMASK1}`
-      ],
+    "base-Sepolia": {
+      url: 'https://base-sepolia.blockpi.network/v1/rpc/public',  // RPC URL for Base Sepolia
+      accounts: [PRIVATE_KEY],
     },
   },
-  defaultNetwork: "hardhat",
   etherscan: {
-    apiKey: "GCTVJFZFESQ8RSGU6VDKDCG8Q4DGSQK3Q1"
-  },
-  customChains: [
-    {
-      network: "base-sepolia",
-      chainId: 84531,
-      urls: {
-        apiURL: "https://optimism-sepolia.blockscout.com/api",
-        browserURL: "https://optimism-sepolia.blockscout.com/",
+    apiKey: {
+      // Any non-empty string can be used for Base Sepolia
+      baseSepolia: "f96ae322-4ab3-490b-b7a2-fdf5d09ec8c2"
+    },
+    customChains: [
+      {
+        network: "base-Sepolia",
+        chainId: 84531, // Base Sepolia chain ID
+        urls: {
+          apiURL: "https://base-sepolia.blockscout.com/api",
+          browserURL: "https://base-sepolia.blockscout.com/",
+        }
       }
-    }
-  ]
-  // sourcify: {
-  //   enabled: true
-  // }
+    ]
+  },
+  sourcify: {
+    enabled: false
+  }
 };
+
+export default config;
